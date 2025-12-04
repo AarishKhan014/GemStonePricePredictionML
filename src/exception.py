27a2bb9b3243b008e5a936364354a3f2894ef29b
Aarish Khan <aarishk140400@gmail.com>
2025-12-04 12:35:28 +0530

    Exception And Logging

--- src/exception.py
+++ src/exception.py
@@ -1,26 +1,21 @@
 import sys
 from src.logger import logging
 
-def error_message_detail(error, error_detail: sys):
+
+def error_message_detail(error, error_detail:sys):
     _, _, exc_tb = error_detail.exc_info()
-    file_name = exc_tb.tb_frame.f_code.co_filename
-
-    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name,
-        exc_tb.tb_lineno,
-        str(error)
-    )
-
-    return error_message
+    file = exc_tb.tb_frame.f_code.co_filename
+    line = exc_tb.tb_lineno
+    return f"Error in [{file}] at line [{line}]: {error}"
 
 class CustomException(Exception):
-
-    def __init__ (self, error_message, error_detail:sys):
-        super().__init__(error_message)
-        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+    def __init__(self, error, error_detail):
+        super().__init__(error)
+        self.error_message = error_message_detail(error, error_detail)
 
     def __str__(self):
         return self.error_message
+
     
 
 '''
